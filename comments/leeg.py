d6cbe223190168f8fa17369c155eb4d9b5a9859b
Jesse Verra <jesseverra@gmail.com>
2023-04-03 21:39:11 +0200

    21:38 3-4-23

---
+++ comments/leeg.py
@@ -0,0 +1,6 @@
+some_number = 5
+type(some_number)
+another_number = 9.81
+type(another_number)
+some_string = 'Hello world'
+type(some_string)
---
+++ comments/main.py
@@ -0,0 +1,19 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+# rechtstreeks van het internet geplukt.
+i = 1
+while i != 6: #kaas
+  print(i)     # meer kaas
+  i += 1
+
+
+""" als een comment ziet het er zo uit: 
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+"""
