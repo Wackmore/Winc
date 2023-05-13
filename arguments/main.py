0076ed7c90223cac4143f28a8ce871e0217b61a8
Jesse Verra <jesseverra@gmail.com>
2023-05-13 21:16:43 +0200

    21:16

--- arguments/main.py
+++ arguments/main.py
@@ -3,14 +3,36 @@ __winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
 __human_name__ = 'arguments'
 
 # Add your code after this line
+mass = 0
 
+def greet(name, template = "Hello, <name>!"):
+        greet = template.replace("<name>",name)
+        return greet
 
-def greet(name , tamplete = 1):
-        if tamplete == 1:
-                greet = f"Hello, {name}"
-                return greet
-        else:
-                greet = F"{tamplete}{name}"
-                return greet
-        
-print(greet("Jesse"))
+def force(mass=0,body="earth"):
+        mass = int(mass)
+        body = str(body)
+
+        planets = {
+        "mercury" : 3.7,
+        "venus"   : 8.9,
+        "earth"   : 9.8,
+        "moon"    : 1.6,
+        "mars"    : 3.7,
+        "jupiter" : 23.1,
+        "saturn"  : 9,
+        "uranus"  : 8.7,
+        "neptune" : 11,
+        "pluto"   : 0.7
+        }       # I am aware that pluto and the moon are not a planet ;)
+
+        bodywaight = planets.get(body)
+        force = mass*bodywaight
+        return force
+
+def pull(m1,m2,d):
+        forceconstant = 6.674*(10**-11)
+        pull = forceconstant*(m1*m2)/(d**2)
+        return pull
+
+print(force(69))
